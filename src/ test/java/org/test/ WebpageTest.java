29a43b0240a0bc7fd6adfc966b6f27ce52e172e5
gokul19082005 <gokulgowda2005@gmail.com>
2026-02-27 12:32:24 +0530

    Added website files for GitHub Pages

---
+++ src/ test/java/org/test/ WebpageTest.java
@@ -0,0 +1,29 @@
+
+        package org.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class WebpageTest {
+    WebDriver driver;
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://github.com/swathivokkaliga-design/gradle-demo.git");
+    }
+    @Test
+    public void titleValidationTest() {
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Tripillar Solutions";
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
+}
